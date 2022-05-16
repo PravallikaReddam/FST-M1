c99d43cc0ef5e203857ee65fd073e12c8cedc856
PravallikaReddam <37142826+PravallikaReddam@users.noreply.github.com>
2022-05-16 11:19:17 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity1.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity1 {
+        public static void main(String[] args) {
+
+            WebDriver driver = new FirefoxDriver();
+            //Open the browser
+            driver.get("https://www.training-support.net");
+            //Close the browser
+            driver.close();
+        }
+
+}
