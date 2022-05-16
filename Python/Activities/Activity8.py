8d48fc2dca6c5edb0bebdf4c3df15c9272e5c635
PravallikaReddam <37142826+PravallikaReddam@users.noreply.github.com>
2022-05-16 11:14:53 +0530

    Add files via upload

---
+++ Python/Activities/Activity8.py
@@ -0,0 +1,12 @@
+List = [1,2,5,5,1]
+
+firstNum = List[0] 
+print ("First Number :" + str(firstNum) )
+
+lastNum = List[-1]
+print ("Last Number :" + str(lastNum))
+
+if(firstNum == lastNum ) :
+    print(True)
+else :
+    print(False)
